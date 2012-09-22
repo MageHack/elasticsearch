6d388f38562bda60079f48bc866c2fee87727aaf
Adrian Duke <adrian@gpmd.co.uk>
2012-09-22 21:47:38 +0100

    Added new observer, fixed attribute extensions for ES

---
+++ app/code/community/Magehack/Elasticsearch/Model/Events/Observer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Observer model
+ *
+ * @category    GPMD
+ * @package     Magehack_Elasticsearch
+ * @copyright   
+ * @license     
+ */
+
+/**
+ *
+ * @category    GPMD
+ * @package     Magehack_Elasticsearch
+ * @author      Carlo Tasca <dev@gpmd.net>
+ */
+class Magehack_Elasticsearch_Model_Events_Observer {
+	/**
+	 * Inject 'Use in Autosuggest' field to add/edit attribute form
+	 * 
+	 * @param Varien_Event_Observer $observer 
+	 */
+	public function adminAttributeEdit(Varien_Event_Observer $observer){
+		$form = $observer->getEvent()->getForm();
+		if($form){
+			$fieldset = $form->getElement('front_fieldset');
+			$fieldset->addField('use_in_autosuggest', 'select', array(
+				'name'     => 'use_in_autosuggest',
+				'label'    => Mage::helper('catalog')->__('Use in Autosuggest'),
+				'title'    => Mage::helper('catalog')->__('Use in Autosuggest'),
+				'values'   => Mage::getModel('adminhtml/system_config_source_yesno')->toOptionArray(),
+				'note'	   => Mage::helper('catalog')->__('Used in Elasticsearch, attribute is used for search box auto-suggestions'),
+			));
+		}
+	}
+}
