b2705ff5a148f8131e9ca45676c0e36dddb90145
Carlo Tasca <carlo.tasca.mail@gmail.com>
2012-09-23 11:59:46 +0100

    some stuff cannot remember

--- app/code/community/Magehack/Elasticsearch/Block/CatalogSearch/Result.php
+++ app/code/community/Magehack/Elasticsearch/Block/CatalogSearch/Result.php
@@ -2,10 +2,34 @@
 class Magehack_Elasticsearch_Block_CatalogSearch_Result extends Mage_CatalogSearch_Block_Result{
 	protected function _prepareLayout()
     {
-    	if(!$this->getRequest()->isXmlHttpRequest()){
+		if(!$this->getRequest()->isXmlHttpRequest()){
     		return parent::_prepareLayout();
     	}else{
             return $this;
         }
     }
+	
+	/**
+	 * Retrieve search result count
+	 *
+	 * @return string
+	 */
+	public function getResultCount() {
+		return $this->_getProductCollection()->getElasticCollectionHitTotal();
+	}
+
+	/**
+	 * Retrieve loaded category collection
+	 *
+	 * @return Mage_CatalogSearch_Model_Resource_Fulltext_Collection
+	 */
+	protected function _getProductCollection() {
+		Mage::helper('elasticsearch')->log(get_class($this) . '::_getProductCollection() invoked');
+		if (is_null($this->_productCollection)) {
+			$this->_productCollection = $this->getListBlock()->getLoadedProductCollection();
+		}
+		$this->_productCollection = $this->getListBlock()->getLoadedProductCollection();
+		return $this->_productCollection;
+	}
+
 }
