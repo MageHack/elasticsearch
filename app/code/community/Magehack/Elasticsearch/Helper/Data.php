57ff8c404e234b38508e26cdcda35cbe0ad376fd
Adrian Duke <adrian@gpmd.co.uk>
2012-09-23 12:28:10 +0100

    Added getquery text

--- app/code/community/Magehack/Elasticsearch/Helper/Data.php
+++ app/code/community/Magehack/Elasticsearch/Helper/Data.php
@@ -491,4 +491,47 @@ class Magehack_Elasticsearch_Helper_Data extends Mage_Core_Helper_Abstract
 		$orFilter->addFilter($visibilityCatalogSearch);
 		return $orFilter;
 	}
+	
+	/**
+	 * Retrieve search query text
+	 *
+	 * @return string
+	 */
+	public function getQueryText()
+	{
+
+		if (is_null($this->_queryText)) {
+			$this->_queryText = $this->_getRequest()->getParam($this->getQueryParamName());
+			if ($this->_queryText === null) {
+				$this->_queryText = '';
+			} else {
+				if (is_array($this->_queryText)) {
+					$this->_queryText = null;
+				}
+				$this->_queryText = trim($this->_queryText);
+				$this->_queryText = Mage::helper('core/string')->cleanString($this->_queryText);
+
+				if (Mage::helper('core/string')->strlen($this->_queryText) > $this->getMaxQueryLength()) {
+					$this->_queryText = Mage::helper('core/string')->substr(
+							$this->_queryText, 0, $this->getMaxQueryLength()
+					);
+					$this->_isMaxLength = true;
+				}
+			}
+		}
+		return $this->_queryText;
+	}
+
+	/**
+	 * Wrapper for getQueryText method
+	 * 
+	 * Just here in case we'll need to extend its functionality in ES
+	 * 
+	 * @return type 
+	 */
+	public function getEqueryText()
+	{
+		return $this->getQueryText();
+	}
+
 }
